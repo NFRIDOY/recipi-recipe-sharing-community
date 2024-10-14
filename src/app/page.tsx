b250129afd0d315363da3705f311c14f04859abd
NF RIDOY <nfridoy@gmail.com>
2024-10-14 17:36:07 +0600

    ✅ [Update] : navber on app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Banner from "@/components/ui/Banner";
-import Navbar from "@/components/ui/Navbar";
 
 export default function Home() {
     return (
         <div className="">
-            <Navbar />
             <Banner />
         </div>
     );
